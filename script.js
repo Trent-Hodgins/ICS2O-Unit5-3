1f3f86ba1292a38ecb5933121493cc4d7b8030c4
TrentHodgins <>
2021-06-02 14:54:44 +0000

    added coments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById('button').addEventListener('click', reportResults)
+//This line tells the code to not activate the command until the "button" is "clicked"//
 
 function reportResults () {
   alert(document.getElementById('input').value)
 }
+//This line tells the code what to do once the button is clicked. 
